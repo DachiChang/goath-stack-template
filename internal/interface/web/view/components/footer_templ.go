c187e3743a4261d2d5dd79e52b5f554c9294bb35
Dachi Chang <dachi.chang@idtechproducts.com>
2025-11-03 11:04:41 +0800

    use daisyUI components

--- internal/interface/web/view/components/footer_templ.go
+++ internal/interface/web/view/components/footer_templ.go
@@ -29,7 +29,7 @@ func Footer() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<footer class=\"flex justify-center py-5\"><p>Copyright &copy; 2025 Goath stack. All rights reserved</p></footer>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<footer class=\"footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4\"><aside><p>Copyright &copy; 2025 Goath stack. All rights reserved</p></aside></footer>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
